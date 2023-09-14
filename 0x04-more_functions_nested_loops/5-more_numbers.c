0394fe4df6189d04745f10a033b79929d769c02b
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-09-14 23:18:20 +0300

    H

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -16,13 +16,12 @@ void more_numbers(void)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			if (i >= 10)
+			if (i > 9)
 			{
 				_putchar(i / 10 + 48);
 
 			}
-			else
-				_putchar(i % 10 + 48);
+			_putchar(i % 10 + 48);
 
 		}
 		_putchar('\n');
