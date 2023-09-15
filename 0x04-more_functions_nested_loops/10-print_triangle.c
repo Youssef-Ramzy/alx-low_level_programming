a9cf31ad5a53e15b91e873ad49a35cc91f9b87dd
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-09-15 15:02:15 +0300

    H

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ *
+ * @size: size of the triangle
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int r = 1; r <= size; r++)
+		{
+		for (int c = 1; c <= size - r; c++)
+		{
+			_putchar(' ');
+		}
+		for (int k = 1; k <= r; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+		}
+	}
+}
