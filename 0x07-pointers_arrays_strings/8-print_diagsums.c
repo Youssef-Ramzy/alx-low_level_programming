36293cb6bbcecb6117f1537903a4efbc05f7777c
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-09-26 04:00:57 +0300

    H

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * print_diagsums - print the sum
+ * print_diagsums - print the sum of two 2d array
+ *
  * @a: input pointer
  * @size: size of the matrix
  *
@@ -10,21 +11,17 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i, a, b;
+	int i, j, k;
 
-	a = 0;
-	b = 0;
+	j = 0;
+	k = 0;
 
 	for (i = 0; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)
-		{
-			a += a[i];
-		}
+			j += a[i];
 		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-		{
-			b += a[i];
-		}
+			k += a[i];
 	}
-	printf("%d, %d\n", a, b);
+	printf("%d, %d\n", j, k);
 }
