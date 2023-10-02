90f6aa4f23c5a0f4a2460895ceda2062a0e2dd28
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-02 19:22:12 +0300

    H

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - a program that multiplies two numbers.
  * @argc: argument count
@@ -7,21 +8,17 @@
 */
 int main(int argc, char *argv[])
 {
-	int i, num;
+	int i, sum;
 
 	if (argc > 1)
 	{
-		for (i = 1; i < argc; i++)
-		{
-			num *= atoi(argv[i]);
-		}
-		printf("%d\n", num);
+		sum = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", sum);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
 	return (0);
 }
