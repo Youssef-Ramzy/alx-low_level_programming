fc5c1e9b8b95407e4b736703940996c7420a61f2
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-02 19:54:55 +0300

    H

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,28 +8,28 @@
 */
 int main(int argc, char *argv[])
 {
-	int i, sum;
+	int i, j, sum = 0;
 
-	if (argc > 1)
+	if (argc < 2)
 	{
-		sum = 0;
-		for (i = 1; i < argc; i++)
-		{
-			if (atoi(argv[i]) > '0' && atoi(argv[i]) <= '9')
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		printf("%d\n", sum);
+		printf("0\n");
+		return (0);
 	}
 	else
 	{
-		printf("0\n");
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; argv[i][j]; j++)
+			{
+				if (!(argv[i][j] >= '0' && argv[i][j] <= '9'))
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+			sum += atoi(argv[i]);
+		}
 	}
+	printf("%d\n", sum);
 	return (0);
 }
