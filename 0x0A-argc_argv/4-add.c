843958bb3b7f2c26e42b993c3608bf4badf83da0
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-02 19:51:18 +0300

    H

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) > 0)
+			if (atoi(argv[i]) > '0' && atoi(argv[i]) <= '9')
 			{
 				sum += atoi(argv[i]);
 			}
