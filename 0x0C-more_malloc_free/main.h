322fcf3868b40496ba2b74f51e4b4062308bddb7
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-06 17:15:20 +0300

    H

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,5 +2,8 @@
 #define MAIN_H
 
 unsigned int _strlen(char *s);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif /* MAIN_H */
