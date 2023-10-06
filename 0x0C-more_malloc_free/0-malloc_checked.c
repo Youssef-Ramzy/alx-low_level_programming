dba16f8026f67d90b5ad5974429634152e77b00f
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-06 15:40:14 +0300

    H

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,14 @@
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: size of memory to aloocate
+ * Return: pointer to allocated memory
+*/
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
