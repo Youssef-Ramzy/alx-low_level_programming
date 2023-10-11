1f1afb8cb0d318a6fba3e4b73ae3b4b5333dbc5d
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-11 23:26:42 +0300

    H

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,17 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 
 void print_name(char *name, void (*f)(char *));
 
-#endif /* MAIN_H */
+/**
+ * enum bool - typedef function for bool
+ * @TRUE: 1
+ * @FALSE: 0
+*/
+typedef enum bool
+{
+	TRUE = 1,
+	FALSE = 0
+} bool;
+
+#endif /* FUNCTION_POINTERS_H */
