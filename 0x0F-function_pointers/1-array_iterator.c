f8fda8e5287dffc7f721dba11414a1b2c4f422dd
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-12 00:15:15 +0300

    H

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	if (array != NULL && action != NULL && size > 0)
+	if (array && action)
 	{
 		for (size_t i = 0; i < size; i++)
 		{
