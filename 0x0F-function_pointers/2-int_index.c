2a5dcf2438dcb2c4738fd697f650ace8077d50b4
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-12 00:47:51 +0300

    H

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stddef.h>
 /**
  * int_index - searches for an integer
  * @array: array of integers
@@ -13,18 +14,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	bool j;
 
-	if (array != NULL && cmp != NULL && size > 0)
+	if (array != NULL && cmp != NULL)
 	{
-		if (size <= 0)
-			return (-1);
 		for (i = 0; i < size; i++)
 		{
-			j = cmp(array[i]);
-			if (j == TRUE)
-				return (i);
+			if (cmp(array[i]))
+				return i;
 		}
 	}
-	return (-1);
+	return -1;
 }
