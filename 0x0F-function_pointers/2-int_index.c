9899908293eaf6e5f47e4ba45660ea8747443a6b
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-12 00:30:11 +0300

    H

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,15 +13,18 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+	bool j;
 
-	if (size <= 0)
-		return (-1);
-	if (array == NULL || cmp == NULL)
-		return (-1);
-	for (i = 0; i < size; i++)
+	if (array != NULL && cmp != NULL && size > 0)
 	{
-		if ((*cmp)(array[i]) == TRUE)
-			return (i);
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			j = cmp(array[i]);
+			if (j == TRUE)
+				return (i);
+		}
 	}
 	return (-1);
 }
