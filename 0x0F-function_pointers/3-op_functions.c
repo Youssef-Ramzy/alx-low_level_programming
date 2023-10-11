6242123661073091946b2d3a5829847a2b210cac
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-12 01:10:05 +0300

    H

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+/**
+ * op_add - adds @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: sum of @a and @b
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts @b from @a
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: difference of @a and @b
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies @a and @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: product of @a and @b
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides @a by @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: quotient of @a and @b
+*/
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - finds the remainder of @a divided by @b
+ *
+ * @a: input 1
+ * @b: input 2
+ *
+ * Return: remainder of @a divided by @b
+*/
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
