4bdc92ffd9c6f897493f86c54af2d38981ecc67e
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-12 00:25:43 +0300

    H

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+/**
+ * int_index - searches for an integer
+ * @array: array of integers
+ * @size: size of array
+ * @cmp: pointer to function
+ * Return: index of first element for which cmp does not return 0
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	bool j;
+
+	if (array != NULL && cmp != NULL && size != 0)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+			j = cmp(array[i]);
+			if (j == TRUE)
+				return (i);
+		}
+	}
+	else
+		return (-1);
+}
