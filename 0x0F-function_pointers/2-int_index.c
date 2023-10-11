5e524528f891ad3bb9917d4ef6ca8f908c041adb
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-12 00:26:50 +0300

    H

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 	bool j;
 
-	if (array != NULL && cmp != NULL && size != 0)
+	if (array != NULL && cmp != NULL && size > 0)
 	{
 		if (size <= 0)
 			return (-1);
