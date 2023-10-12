6df09ec60efcf3f1d287e37c21b08942fdc42eaa
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-13 01:53:28 +0300

    H

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,15 +1,14 @@
-#include <stdarg.h>
 #include <stdio.h>
-#include "variadic_functions.h"
+#include <stdarg.h>
 /**
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
  * Return: void
-*/
+ */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0, j = 0;
+	unsigned int i = 0, j;
 	char *str;
 	func_printer print[] = {
 		{"c", print_char},
@@ -36,4 +35,45 @@ void print_all(const char * const format, ...)
 	}
 	printf("\n");
 	va_end(args);
+}
+/**
+ * print_char - prints a character
+ * @arg: character to be printed
+ * Return: void
+ */
+void print_char(va_list arg)
+{
+	printf("%c", va_arg(arg, int));
+}
+/**
+ * print_int - prints an integer
+ * @arg: integer to be printed
+ * Return: void
+ */
+void print_int(va_list arg)
+{
+	printf("%d", va_arg(arg, int));
+}
+/**
+ * print_float - prints a float
+ * @arg: float to be printed
+ * Return: void
+ */
+
+void print_float(va_list arg)
+{
+	printf("%f", va_arg(arg, double));
+}
+/**
+ * print_string - prints a string
+ * @arg: string to be printed
+ * Return: void
+ */
+void print_string(va_list arg)
+{
+	char *str = va_arg(arg, char*);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
 }
