90c38d6098f7ed0328e0921c4cdae620be488159
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-13 02:08:18 +0300

    H

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,17 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
-#include <string.h>
-/**
- * main - Entry point
- * @argc: Number of arguments
- * @argv: Array of arguments
- * Return: 0 (Success)
-*/
+#include <stdio.h>
+#include <string.h> /*for atoi*/
+#include <stdlib.h> /*for exit*/
 
+/**
+ * main - performs simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if successful
+ * If the number of arguments is wrong, print Error, followed by a new line,
+ * and exit with the status 98
+ * If the operator is none of the above, print Error, followed by a new line,
+ * and exit with the status 99
+ * If the user tries to divide (/ or %) by 0, print Error, followed by a new
+ * line, and exit with the status 100
+*/
 int main(int argc, char *argv[])
 {
-	int (*f)(int, int);
+	int num1, num2;
+	char *operator;
 
 	if (argc != 4)
 	{
@@ -19,15 +27,23 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	f = get_op_func(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
 
-	if (f == NULL)
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+	if ((*operator == 47 || *operator == 37) && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(operator)(num1, num2));
 
 	return (0);
 }
