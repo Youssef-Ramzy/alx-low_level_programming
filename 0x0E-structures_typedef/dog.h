cc0c411b385d350a01c94830595d1239d72fcef1
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-10-13 19:48:13 +0300

    H

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - dog structure
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: dog structure
+ */
 struct dog
 {
 	char *name;
