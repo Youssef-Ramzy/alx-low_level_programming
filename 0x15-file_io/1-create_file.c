30ab815e41f321676b475f037e086e825943005e
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-11-14 00:38:10 +0200

    v1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * create_file - creates a file
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int file, check, size = 0;
+
+	if (filename == NULL)
+		return (0);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0400 | 0200);
+	if (file == -1)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[size] != NULL)
+			size++;
+		check = write(file, text_content, size);
+		if (check == -1)
+			return (-1);
+		close(file);
+	}
+	return (0);
+}
