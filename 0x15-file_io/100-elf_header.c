cb83146863689155b9b5c23fb2a7323a5f3fe592
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-11-14 03:13:45 +0200

    v3

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,57 +1,85 @@
 #include "main.h"
 /**
- * handle_error - handles errors and exits program
- * @error_message: error message to print
- * @file_descriptor: file descriptor to close
- */
-void handle_error(const char *error_message, int file_descriptor)
+ * check_if_elf - check if the file is an ELF file
+ * @e_ident: the ELF header
+ * Return: nothing
+*/
+void check_if_elf(unsigned char *e_ident)
 {
-	if (file_descriptor != -1)
+	int index = 0;
+	char *error_msg = "Error: Not an ELF file\n";
+
+	while (index < 4)
 	{
-		close(file_descriptor);
+		if (e_ident[index] != 127 &&
+			e_ident[index] != 'E' &&
+			e_ident[index] != 'L' &&
+			e_ident[index] != 'F') /*0x7F is 127 in ASCII*/
+		{
+			write(STDERR_FILENO, error_msg, strlen(error_msg));
+			exit(98);
+		}
+		index++;
 	}
-	dprintf(STDERR_FILENO, "%s\n", error_message);
-	exit(98);
 }
 /**
- * main - program entry point
- * @argument_count: number of arguments passed to program
- * @argument_values: array of pointers to argument strings
- * Return: 0 on success, 98 on failure
+ * close_file - close the file
+ * @elf_file: the file descriptor
+ * Return: nothing
 */
-int main(int __attribute__((__unused__)) argument_count,
-	 char *argument_values[])
+void close_file(int elf_file)
 {
-	Elf64_Ehdr elf_header;
-	int elf_file_descriptor;
-
-	elf_file_descriptor = open(argument_values[1], O_RDONLY);
-	if (elf_file_descriptor == -1)
+	if (close(elf_file) == -1)
 	{
-		handle_error("Error: Can't read file", -1);
+		char *error_msg = "Error: Can't close fd\n";
+
+		write(STDERR_FILENO, error_msg, strlen(error_msg));
+		exit(98);
+	}
+}
+/**
+ * main - the main function
+ * @argc: the number of arguments
+ * @argv: the arguments
+ * Return: 0
+*/
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	Elf64_Ehdr *elf;
+	int elf_file, read_file;
+
+	elf_file = open(argv[1], O_RDONLY);
+	if (elf_file == -1)
+	{
+		char *error_msg = "Error: Can't read file\n";
+
+		write(STDERR_FILENO, error_msg, strlen(error_msg));
+		exit(98);
 	}
 
-	if (read(elf_file_descriptor, &elf_header, sizeof(Elf64_Ehdr))
-		!= sizeof(Elf64_Ehdr))
+	elf = malloc(sizeof(Elf64_Ehdr));
+	if (elf == NULL)
 	{
-		handle_error("Error: Can't read file", elf_file_descriptor);
+		close_file(elf_file);
+		free(elf);
+		char *error_msg = "Error: Can't read file\n";
+
+		write(STDERR_FILENO, error_msg, strlen(error_msg));
+		exit(98);
 	}
 
-	check_if_elf(elf_header.e_ident);
-	printf("ELF Header:\n");
-	print_magic(elf_header.e_ident);
-	print_class(elf_header.e_ident);
-	print_data(elf_header.e_ident);
-	print_version(elf_header.e_ident);
-	print_osabi(elf_header.e_ident);
-	print_abi(elf_header.e_ident);
-	print_type(elf_header.e_type, elf_header.e_ident);
-	print_entry(elf_header.e_entry, elf_header.e_ident);
-
-	if (close(elf_file_descriptor) == -1)
+	read_file = read(elf_file, elf, sizeof(Elf64_Ehdr));
+	if (read_file == -1)
 	{
-		handle_error("Error: Can't close fd", elf_file_descriptor);
-	}
+		free(elf);
+		close_file(elf_file);
+		char *error_msg = "Error: No such file\n";
 
+		write(STDERR_FILENO, error_msg, strlen(error_msg));
+		exit(98);
+	}
+	check_if_elf(elf->e_ident);
+	free(elf);
+	close_file(elf_file);
 	return (0);
 }
