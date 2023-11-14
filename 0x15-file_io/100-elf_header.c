7f0e53279518b5cf2b3239e51a722889af3f8a5d
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-11-14 02:53:39 +0200

    v1

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,72 +1,32 @@
 #include "main.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <elf.h>
-
-void print_elf_header(Elf64_Ehdr *header);
-void check_elf_file(Elf64_Ehdr *header);
 /**
- * main - displays the information contained in the ELF header at the
- *    start of an ELF file.
+ * main - displays the information contained in the ELF header at the start of
+ *        an ELF file.
+ *
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0 on success
-*/
-int main(int argc, char **argv)
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
 {
-	int fd;
-	Elf64_Ehdr header;
+	int fd, rd;
+	char buf[ELF_DATA];
+	ssize_t sz;
 
 	if (argc != 2)
-	{
-		fprintf(stderr, "Usage: elf_header elf_filename\n");
-		exit(98);
-	}
-
+		dprintf(STDERR_FILENO, "Usage: %s elf_filename\n", argv[0]), exit(98);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
-	{
-		fprintf(stderr, "Error: Can't read file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", argv[1]), exit(98);
+	rd = read(fd, buf, ELF_DATA);
+	if (rd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
-	}
-
-	if (read(fd, &header, sizeof(header)) != sizeof(header))
-	{
-		fprintf(stderr, "Error: Failed to read ELF header\n");
-		exit(98);
-	}
-
-	check_elf_file(&header);
-	print_elf_header(&header);
-
+	sz = write(STDOUT_FILENO, buf, rd);
+	if (sz == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to STDOUT\n"), exit(98);
 	close(fd);
 	return (0);
-}
-/**
- * check_elf_file - checks if the file is an ELF file
- * @header: pointer to the ELF header struct
- * Return: void
-*/
-void check_elf_file(Elf64_Ehdr *header)
-{
-	if (header->e_ident[EI_MAG0] != ELFMAG0 ||
-		header->e_ident[EI_MAG1] != ELFMAG1 ||
-		header->e_ident[EI_MAG2] != ELFMAG2 ||
-		header->e_ident[EI_MAG3] != ELFMAG3)
-	{
-		fprintf(stderr, "Error: Not an ELF file\n");
-		exit(98);
-	}
-}
-/**
- * print_elf_header - prints the ELF header information
- * @header: pointer to the ELF header struct
- * Return: void
-*/
-void print_elf_header(Elf64_Ehdr *header)
-{
-	printf("Magic:   ");
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
 #include <elf.h>
+#define ELF_DATA 16
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
