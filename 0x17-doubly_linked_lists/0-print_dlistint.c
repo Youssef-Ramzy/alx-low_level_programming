b86a9e6cd8b8063b93c37956a0253c965850063e
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-11 00:12:25 +0200

    v0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * print_dlistint - pirnt every element
+ * @h: pointer to the head
+ * Return: number of element
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t n = 0;
+	const dlistint_t *temp = NULL;
+
+	temp = h;
+	if (temp == NULL)
+		exit(EXIT_FAILURE);
+	else
+	{
+		while (temp != NULL)
+		{
+			printf("%d\n", temp->n);
+			temp = temp->next;
+			n++;
+		}
+		return (n);
+	}
+}
