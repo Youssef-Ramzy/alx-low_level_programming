fb456bac1daf1796657ce005661017305ba8187f
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-11 02:58:00 +0200

    v4

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,7 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		temp = temp->next;
 	}
 
-	if (i == idx) // Check if the new node should be added at the end of the list
+	if (i == idx)
 	{
 		new_node->prev = temp;
 		temp->next = new_node;
