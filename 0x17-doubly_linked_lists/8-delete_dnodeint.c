f7a3e617f688c24bf527e548b502ea0b7933adf5
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-11 03:06:17 +0200

    v1

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -9,11 +9,11 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	if (*head == NULL)
-		return (-1);
-
+	unsigned int i = 0;
 	dlistint_t *current = *head;
 
+	if (*head == NULL)
+		return (-1);
 	if (index == 0)
 	{
 		*head = current->next;
@@ -22,9 +22,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		free(current);
 		return (1);
 	}
-
-	unsigned int i = 0;
-
 	while (current != NULL && i < index)
 	{
 		current = current->next;
