73fb0f7f47810eddb29a292baafe6d71b2156f1a
Youssef-Ramzy <yousseframzyvd@gmail.com>
2023-12-11 02:26:38 +0200

    v1

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -12,8 +12,6 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	if (head == NULL)
 		return (NULL);
-	if (index < 0)
-		return (NULL);
 	while (n < index)
 	{
 		if (temp == NULL)
