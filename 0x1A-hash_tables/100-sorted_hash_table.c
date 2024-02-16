101d57874df0f48c354ee1e94151f28c53c8c7d1
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-02-16 11:29:00 +0200

    v0

---
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * shash_table_create - creates a sorted hash table
+ * @size: size of the array
+ * Return: pointer to the newly created hash table
+ */
+shash_table_t *shash_table_create(unsigned long int size)
+{
+	shash_table_t *ht;
+
+	if (size == 0)
+		return (NULL);
+
+	ht = malloc(sizeof(shash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = calloc(size, sizeof(shash_node_t *));
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	ht->shead = NULL;
+	ht->stail = NULL;
+
+	return (ht);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -32,7 +32,41 @@ typedef struct hash_table_s
 	 unsigned long int size;
 	 hash_node_t **array;
 } hash_table_t;
-
+/**
+ * struct shash_node_s - Node of a sorted hash table
+ *
+ * @key: The key, string
+ * The key is unique in the HashTable
+ * @value: The value corresponding to a key
+ * @next: A pointer to the next node of the List
+ * @sprev: A pointer to the previous node in the sorted linked list
+ * @snext: A pointer to the next node in the sorted linked list
+ */
+typedef struct shash_node_s
+{
+	 char *key;
+	 char *value;
+	 struct shash_node_s *next;
+	 struct shash_node_s *sprev;
+	 struct shash_node_s *snext;
+} shash_node_t;
+/**
+ * struct shash_table_s - Sorted hash table data structure
+ *
+ * @size: The size of the array
+ * @array: An array of size @size
+ * Each cell of this array is a pointer to the first node of a linked list,
+ * because we want our HashTable to use a Chaining collision handling
+ * @shead: A pointer to the first node of the sorted linked list
+ * @stail: A pointer to the last node of the sorted linked list
+ */
+typedef struct shash_table_s
+{
+	 unsigned long int size;
+	 shash_node_t **array;
+	 shash_node_t *shead;
+	 shash_node_t *stail;
+} shash_table_t;
 /* The prototype of the fuctions */
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
@@ -41,5 +75,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+shash_table_t *shash_table_create(unsigned long int size);
 
 #endif
