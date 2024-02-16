76075401234f495e55b0946f79af8111ff0f7a26
Youssef-Ramzy <yousseframzyvd@gmail.com>
2024-02-16 11:16:35 +0200

    v1

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,10 +1,11 @@
-#include "hash_table.h"
+#include "hash_tables.h"
 
 /**
  * hash_table_get - function that retrieves a value associated with a key
  * @ht: hash table you want to look into
  * @key: key you are looking for
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ * Return: the value associated with the element
+ * , or NULL if key couldn’t be found
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
